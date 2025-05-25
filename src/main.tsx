ebef6c898b00387c4155976626ea3f78b3955e5d
Red P <red.parker.red@gmail.com>
2025-05-25 13:16:13 +0100

    Theme issue fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import './index.css'
 import App from './App'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
-import autumnTheme from './theme'
+import getTheme from './theme'
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Root element not found');
 createRoot(container).render(
   <StrictMode>
-    <ThemeProvider theme={autumnTheme}>
+    <ThemeProvider theme={getTheme('light')}>
       <CssBaseline />
       <App />
     </ThemeProvider>
